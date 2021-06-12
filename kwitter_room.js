04523cad446cfee66aaadeda297a81de362cc499
Bed1234 <76521252+Bed1234@users.noreply.github.com>
2021-06-12 13:34:31 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -14,10 +14,6 @@ var firebaseConfig = {
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
 
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-       Room_names = childKey;
-      //Start code
 
-      //End code
-      });});}
-getData();
+    user_name = localStorage.getItem("user_name");
+    document.getElementById("user_name").innerHTML = "Welcome "+user_name+" ! ";
